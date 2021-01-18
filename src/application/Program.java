0fb8b6e104bc35304d0e5896fe55389b539a2c56
Okzser <s.reszkojr@gmail.com>
2021-01-17 21:51:39 -0300

    Implemented moving pieces system

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,26 @@ package application;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.print("\n Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+
+			System.out.print("\n Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 }
